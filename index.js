f08bfc621f7e90dfa39a73b251375ae5ed4e366f
elitemind12 <makyaodaniel55@gmail.com>
2022-10-11 11:08:31 +0300

    configuring session

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const methodOverride = require('method-override');
 const campgroundRouters = require('./routes/campgrounds');
 const reviewRouters = require('./routes/review');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
 
 
 
@@ -29,7 +30,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-
+const sessionConfig = {
+    secret: 'confidential',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
 
 
 
